e42ee4525dd2347ba4fa9ac25fdf36efaae35a0f
Lukas Gienapp <lukas.gienapp@uni-leipzig.de>
2021-02-25 22:00:02 +0100

    Bug fixes

--- run.sh
+++ run.sh
@@ -16,12 +16,16 @@ done
 set -e
 set -x
 
-docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.10.1
+rm -rf venv
+
+docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.10.1
 
 python3 -m venv venv
 source venv/bin/activate
 pip install -U pip
 pip install -r requirements.txt
 
-python3 05-index.py --data data/index.parquet
-python3 06-query.py --topic_path $in_dir/topics.xml --output_path $out_dir/run.txt
+python3 05-Index.py --data data/index.parquet
+python3 06-Query.py --topic_path $in_dir/topics.xml --output_path $out_dir/run.txt
+
+docker container stop $(docker container list -q)
